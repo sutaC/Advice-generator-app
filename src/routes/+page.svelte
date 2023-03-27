d7b5b4a5d174a1426bb83c15daffe4c6087b0cf3
Antoni Siekański <catus.pl@gmail.com>
2023-03-27 09:43:05 +0200

    Innitial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+
+
+
+</script>
+<!-- ---  -->
+
+
+<main> 
+
+    <h1>ADVICE #177</h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, architecto.
+    </p>
+
+    <div class="devider"></div>
+
+</main>
+
+
+<!-- ---  -->
+<style>
+
+
+
+</style>
